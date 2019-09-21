fd65b94f92675a18edcc80c57dd76b07140d42f8
artem_sheva <Artemshevchuk2097@gmail.com>
2019-09-21 08:40:17 +0300

    Final

--- ConsoleAppInteger/Program.cs
+++ ConsoleAppInteger/Program.cs
@@ -8,8 +8,29 @@ namespace ConsoleAppInteger
 {
     class Program
     {
+        public static double EnterValue()
+        {
+            Console.WriteLine("Enter temperature in Celsium:");
+            string value = Console.ReadLine();
+            double t_cels = double.Parse(value);
+            return t_cels;
+        }
+        public static double  Temperature_conversion(double t_cels)
+        {
+            double t_far = (t_cels * (5 / 9)) + 32;
+            return t_far;
+        }
+
+        public static void Print(double t_far)
+        {
+            Console.WriteLine("t_far = " + t_far);
+        }
         static void Main(string[] args)
         {
+            double t_cels = EnterValue();
+            double t_far = Temperature_conversion(t_cels);
+            Print(t_far);
+
         }
     }
 }
